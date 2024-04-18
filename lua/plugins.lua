4eb4a032e04509b3347e951149b8ed972daf7897
KhanhIceTea <khanhicetea@gmail.com>
2024-04-18 22:21:36 +0700

    terminal

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -263,5 +263,45 @@ require("lazy").setup({
         init = function()
             require("spectre").setup {}
         end,
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        keys = {
+            {
+                "<leader>-",
+                mode = { "n" },
+                "<cmd>ToggleTerm size=40 direction=float<CR>",
+                desc = "Toogle terminal window"
+            },
+        },
+        init = function()
+            require("toggleterm").setup {}
+        end,
+        config = function()
+            local Terminal = require('toggleterm.terminal').Terminal
+
+            local lazygit  = Terminal:new({
+                cmd = "lazygit",
+                dir = "git_dir",
+                direction = "float",
+                float_opts = {
+                    border = "double",
+                },
+                on_open = function(term)
+                    vim.cmd("startinsert!")
+                    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+                end,
+                on_close = function(term)
+                    vim.cmd("startinsert!")
+                end,
+            })
+
+            function _lazygit_toggle()
+                lazygit:toggle()
+            end
+
+            vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+        end
     }
 })
