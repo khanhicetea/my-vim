48440e12d751713082391bbc9ff5270e9b760528
KhanhIceTea <khanhicetea@gmail.com>
2024-04-28 23:33:19 +0700

    config cmp key maps

--- lua/mods/coding/cmp.lua
+++ lua/mods/coding/cmp.lua
@@ -10,31 +10,27 @@ return {
     opts = function()
         local cmp = require("cmp")
         local defaults = require("cmp.config.default")()
+
         return {
             completion = {
                 completeopt = "menu,menuone,noinsert",
             },
             mapping = cmp.mapping.preset.insert({
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
                 ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-CR>"] = function(fallback)
-                    cmp.abort()
-                    fallback()
-                end,
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "nvim_lsp_signature_help" },
                 { name = "luasnip" },
-                { name = "path" },
             }, {
                 { name = "buffer" },
+                { name = "path" },
             }),
             snippet = {
                 expand = function(args)
