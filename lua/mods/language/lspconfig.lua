8add7c5f1086377f0f1e7b4de50e7abb6c6b1fa2
KhanhIceTea <khanhicetea@gmail.com>
2024-04-30 18:41:17 +0700

    support ts and volar language server lsp

--- lua/mods/language/lspconfig.lua
+++ lua/mods/language/lspconfig.lua
@@ -1,42 +1,70 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" },
-    },
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup()
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup()
 
-        require("mason-lspconfig").setup_handlers({
-            function(server_name) -- default handler (optional)
-                require("lspconfig")[server_name].setup({})
-            end,
-            ["intelephense"] = function()
-                require("lspconfig")["intelephense"].setup({
-                    init_options = {
-                        licenceKey = os.getenv("INTELEPHENSE_KEY"),
-                    },
-                    settings = {
-                        intelephense = {
-                            filetypes = { "php", "blade" },
-                            files = {
-                                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
-                                maxSize = 5000000,
-                            },
-                            telemetry = {
-                                enabled = false,
-                            },
-                            completion = {
-                                fullyQualifyGlobalConstantsAndFunctions = false,
-                            },
-                            phpdoc = {
-                                returnVoid = false,
-                            },
-                        },
-                    },
-                })
-            end,
-        })
-    end,
+		local mason_registry = require("mason-registry")
+
+		require("mason-lspconfig").setup_handlers({
+			function(server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup({})
+			end,
+			["intelephense"] = function()
+				require("lspconfig")["intelephense"].setup({
+					init_options = {
+						licenceKey = os.getenv("INTELEPHENSE_KEY"),
+					},
+					settings = {
+						intelephense = {
+							filetypes = { "php", "blade" },
+							files = {
+								associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+								maxSize = 5000000,
+							},
+							telemetry = {
+								enabled = false,
+							},
+							completion = {
+								fullyQualifyGlobalConstantsAndFunctions = false,
+							},
+							phpdoc = {
+								returnVoid = false,
+							},
+						},
+					},
+				})
+			end,
+			["tsserver"] = function()
+				-- Hybrid mode
+				local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+					.. "/node_modules/@vue/language-server"
+
+				require("lspconfig")["tsserver"].setup({
+					init_options = {
+						plugins = {
+							{
+								name = "@vue/typescript-plugin",
+								location = vue_language_server_path,
+								languages = { "vue" },
+								-- configNamespace = "typescript",
+								-- enableForWorkspaceTypeScriptVersions = true,
+							},
+						},
+					},
+					filetypes = {
+						"typescript",
+						"javascript",
+						"javascriptreact",
+						"typescriptreact",
+						"vue",
+					},
+				})
+			end,
+		})
+	end,
 }
