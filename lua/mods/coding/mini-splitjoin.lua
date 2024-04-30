706747a2e22df8a0c9d6efa576f8f87aef10e74f
KhanhIceTea <khanhicetea@gmail.com>
2024-04-30 18:51:57 +0700

    ensured install mason

--- lua/mods/coding/mini-splitjoin.lua
+++ lua/mods/coding/mini-splitjoin.lua
@@ -1,7 +1,7 @@
 return {
-    'echasnovski/mini.splitjoin',
-    version = '*',
-    init = function()
-        require("mini.splitjoin").setup({})
-    end,
+	"echasnovski/mini.splitjoin",
+	version = "*",
+	init = function()
+		require("mini.splitjoin").setup({})
+	end,
 }
