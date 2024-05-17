d91d2fbef620bdf41f2b75e7713dc05ebfd225c7
KhanhIceTea <khanhicetea@gmail.com>
2024-05-17 22:08:54 +0700

    fix barbar

--- lua/mods/ui/barbar.lua
+++ lua/mods/ui/barbar.lua
@@ -1,17 +1,16 @@
 return {
-	"romgrk/barbar.nvim",
-	dependencies = {
-		"lewis6991/gitsigns.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	keys = {
-		{ "<C-h>", "<cmd>BufferPrevious<cr>", desc = "Prev buffer line" },
-		{ "<C-l>", "<cmd>BufferNext<cr>", desc = "Next buffer line" },
-		{ "<Leader>bo", "<cmd>BufferCloseAllButCurrent<cr>", desc = "Close other buffer lines" },
-		{ "<Leader>bd", "<cmd>BufferClose<cr>", desc = "Close current buffer line" },
-	},
-	init = function()
-		vim.g.barbar_auto_setup = true
-	end,
-	opts = {},
+    "romgrk/barbar.nvim",
+    dependencies = {
+        "lewis6991/gitsigns.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+        { "<C-h>",      "<cmd>BufferPrevious<cr>",           desc = "Prev buffer line" },
+        { "<C-l>",      "<cmd>BufferNext<cr>",               desc = "Next buffer line" },
+        { "<Leader>bo", "<cmd>BufferCloseAllButCurrent<cr>", desc = "Close other buffer lines" },
+        { "<Leader>bd", "<cmd>BufferClose<cr>",              desc = "Close current buffer line" },
+    },
+    init = function()
+        require("barbar").setup({})
+    end,
 }
