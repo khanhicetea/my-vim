46d2e33abdb9c4a6025e22773e7c782b225a8f47
KhanhIceTea <khanhicetea@gmail.com>
2024-07-09 12:24:09 +0700

    define inteliphense file exclude local packages folder

--- lua/mods/language/lspconfig.lua
+++ lua/mods/language/lspconfig.lua
@@ -60,6 +60,19 @@ return {
                             files = {
                                 associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
                                 maxSize = 5000000,
+                                exclude = {
+                                    "**/.git/**",
+                                    "**/.svn/**",
+                                    "**/.hg/**",
+                                    "**/CVS/**",
+                                    "**/.DS_Store/**",
+                                    "**/node_modules/**",
+                                    "**/bower_components/**",
+                                    "**/vendor/**/{Tests,tests}/**",
+                                    "**/.history/**",
+                                    "**/vendor/**/vendor/**",
+                                    "**/packages/**",
+                                },
                             },
                             telemetry = {
                                 enabled = false,
