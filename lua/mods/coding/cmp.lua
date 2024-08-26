31f4e62db4c0946595f5f9823198871846d395af
KhanhIceTea <khanhicetea@gmail.com>
2024-08-26 20:50:14 +0700

    php

--- lua/mods/coding/cmp.lua
+++ lua/mods/coding/cmp.lua
@@ -34,7 +34,7 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
-                { name = "nvim_lsp_signature_help" },
+                -- { name = "nvim_lsp_signature_help" },
                 { name = "luasnip" },
             }, {
                 { name = "buffer" },
